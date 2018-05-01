08b65c6df052da9bac684411aa7bd4c15c2f4eb5
tc40 <tc40@hdomain01.local>
2018-05-01 14:06:13 +0900

    test commit

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
     $dsn = 'mysql:dbname=yoshizawa;host=localhost;charset=utf8';
     $user = 'yoshizawa';
-    $password = 'webpgm';
+    $password = '';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
