eff537ce6d968c48af0db3a6a486e39b786eca85
Wakana Yoshizawa <wakanayoshizawa@gmail.com>
2018-05-07 11:21:44 +0900

    add delete and disp

--- staff_branch.php
+++ staff_branch.php
@@ -1,5 +1,11 @@
 <?php
 
+    if (isset($_POST['add'])) {
+        print('追加ボタンが押されました');
+        header('Location: staff_add.php');
+        exit();
+    }
+
     $staff_code = @$_POST['staffcode'] ?: '';
 
     if (empty($staff_code)) {
@@ -8,6 +14,12 @@
     }
 
     print($staff_code.'番のスタッフコードを選びました<br />');
+
+    if (isset($_POST['disp'])) {
+        print('参照ボタンが押されました');
+        header('Location: staff_disp.php?staffcode='.$staff_code);
+        exit();
+    }
 
     if (isset($_POST['edit'])) {
         print('修正ボタンが押されました');
---
+++ staff_delete_done.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>よしざわ農園</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+            
+        //try{        
+            $staff_code = $_POST['code'];
+
+            include 'database.php';
+            $sql = 'DELETE FROM mst_staff WHERE code=?';
+            $stmt = $dbh->prepare($sql);
+            $data[] = $staff_code;
+            $stmt->execute($data);
+            $dbh = null;
+
+        /*}
+        catch(Exception $e){
+            print('ただいま障害により大変ご迷惑をお掛けしております。');
+            exit();
+        }*/
+    ?>
+    削除しました。<br /><br />
+    <a href="staff_list.php">戻る</a>
+</body>
+</html>
