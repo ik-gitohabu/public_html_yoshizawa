3f54e2e1224c25595a2d9f0db428c8c9579de181
Wakana Yoshizawa <wakanayoshizawa@gmail.com>
2018-05-16 10:59:12 +0900

    gitignore

--- database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $dsn = 'mysql:dbname=yoshizawa;host=localhost;charset=utf8';
-    $user = 'yoshizawa';
-    $password = '';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
