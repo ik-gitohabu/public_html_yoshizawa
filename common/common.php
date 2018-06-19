4a368242d5810b56a26b63d2e84980c038b84472
tc40 <tc40@hdomain01.local>
2018-06-19 16:08:57 +0900

    testコード追加

--- common/common.php
+++ common/common.php
@@ -25,6 +25,7 @@
 
     function sanitize($before) {
 
+        $after = array();
         foreach($before as $key => $value) {
             $after[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
         }
--- test.php
+++ test.php
@@ -1,16 +1,38 @@
 <?php
 
-    print('test');
+    //array_splice demo
+    $shori[]=5;
+    $shori[]=4;
+    $shori[]=3;
+    $shori[]=2;
+    $shori[]=1;
+    $shori[]=0;
+    print('処理前<br />');
+    var_dump($shori);
+    print('<br /><br />');
+
+    $kiritori = array_splice($shori, 3, 2);
+    print('処理後<br />');
+    var_dump($shori);
+    print('<br />');
+    print('処理後きりとり<br />');
+    var_dump($kiritori);
+    print('<br /><br />');
+
+    //php print demo
+    /*print('test');
 
     $test = 'test';
     $a = 1;
     $b = 3;
 
+    //文字列連結
     print($test.$a);
+    print('<br />');
+    //計算
     print($a + $b);
+    print('<br />');*/
 
 ?>
 
-<b>aaaaa</b>
-
-<?=$test?>
+<!--<?=$test?>-->
