30ee831fcbf0efa11c71e2fe6c8f193344fce1ab
unknown <TC40@dn2-001.local>
2018-10-11 15:53:42 +0900

    first commit

---
+++ test2.php
@@ -0,0 +1,21 @@
+<?php
+    $test = 'hoge';
+    $a = 9;
+
+    //print($test);
+    /*print $test;
+    print 'fuga';*/
+
+    print $test.$a.'fuga';
+    print '<br />';
+    print ($a + 7 - 3 * 2) / 2;
+
+    $test2[] = 'foo';
+    $test2[] = 'bar';
+
+    /*print_r($test2);
+    var_dump($test2);*/
+?>
+
+<!--<b><?php print $test; ?></b>
+<b><?=$test?></b>-->
